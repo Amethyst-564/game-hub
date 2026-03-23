289ffef2e4391fe42418f222cbaae5655c0ed429
Amethyst-564 <466166377@qq.com>
2026-03-23 21:15:20 +0800

    add genre skeleton

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,22 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Skeleton, SkeletonText, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres"
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  if (error) return null;
   return (
     <List>
+      {isLoading && skeletons.map(skeleton =>
+        <ListItem key={skeleton} paddingY='5px'>
+          <HStack>
+            <Skeleton height='32px' width='32px' borderRadius={8} />
+            <SkeletonText flex="1" fontSize='lg' />
+          </HStack>
+        </ListItem>
+      )}
       {data.map(genre =>
         <ListItem key={genre.id} paddingY='5px'>
           <HStack>
