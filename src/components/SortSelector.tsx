94defb5daa55dea5fa2c18dd2729ac7b24018024
Amethyst-564 <466166377@qq.com>
2026-03-24 20:15:19 +0800

    sort games

---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,33 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
+import { BsChevronDown } from 'react-icons/bs'
+
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  selectedSortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, selectedSortOrder }: Props) => {
+  const sortOrders = [
+    { value: '', label: 'Relevance' },
+    { value: '-added', label: 'Date added' },
+    { value: 'name', label: 'Name' },
+    { value: '-released', label: 'Release date' },
+    { value: '-metacritic', label: 'Popularity' },
+    { value: '-rating', label: 'Average rating' }
+  ];
+
+  const currentSortOrder = sortOrders.find(order => order.value === selectedSortOrder)?.label
+
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Order by: {currentSortOrder || 'Relevance'}
+      </MenuButton>
+      <MenuList>
+        {sortOrders.map(order => <MenuItem key={order.value} onClick={() => onSelectSortOrder(order.value)}>{order.label}</MenuItem>)}
+      </MenuList>
+    </Menu>
+  )
+}
+
+export default SortSelector
