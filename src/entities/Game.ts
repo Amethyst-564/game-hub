90f8d7e54859b342ecad6d18c27a533dd6a03ae7
Amethyst-564 <466166377@qq.com>
2026-04-02 21:49:51 +0800

    build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,10 +1,14 @@
+import type { Genre } from "./Genre";
 import type { Platform } from "./Platform";
+import type { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   slug: string;
   description_raw: string;
   name: string;
+  genres: Genre[];
+  publishers: Publisher[];
   background_image: string;
   parent_platforms: { platform: Platform; }[];
   metacritic: number;
