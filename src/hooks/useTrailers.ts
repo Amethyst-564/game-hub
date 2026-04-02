3608b352968ad20d63289b13cc851ab1af843fef
Amethyst-564 <466166377@qq.com>
2026-04-02 22:17:13 +0800

    build game trailer

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import type { Trailer } from "../entities/Trailer";
+import APIClient from "../services/api-client";
+
+
+const useTrailers = (id: number) => {
+  const apiClient = new APIClient<Trailer>(`/games/${id}/movies`);
+
+  return useQuery({
+    queryKey: ['trailers', id],
+    queryFn: apiClient.getAll
+  })
+};
+
+export default useTrailers;
