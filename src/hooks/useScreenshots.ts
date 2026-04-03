a078f4d810c667a2b98c517415ee5572b79dabab
Amethyst-564 <466166377@qq.com>
2026-04-03 19:17:50 +0800

    add game screenshots

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,15 @@
+
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/api-client";
+import type { Screenshot } from "../entities/Screenshot";
+
+const useScreenshots = (id: number) => {
+  const apiClient = new APIClient<Screenshot>(`/games/${id}/screenshots`)
+
+  return useQuery({
+    queryKey: ['screenshots', id],
+    queryFn: apiClient.getAll
+  })
+};
+
+export default useScreenshots;
